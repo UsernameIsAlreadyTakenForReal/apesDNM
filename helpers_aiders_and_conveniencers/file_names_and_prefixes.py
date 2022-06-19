3c527ec6d23743428e632cc6b386dc5125ffc4c7
Mr. Chis, innit <radu.cislianu@stud.acs.upb.ro>
2022-06-19 13:31:33 +0300

    Further folder structure. File with files info. Beginning unsupervised learning

---
+++ helpers_aiders_and_conveniencers/file_names_and_prefixes.py
@@ -0,0 +1,20 @@
+"""
+This file contains miscellaneous information, such as the path names
+"""
+
+class files_info:
+    shoe_ids = ["5206", "5651", "7051", "11597"]
+    watch_ids = ['3151', '4552', '4553']
+    survey_ids = ['3951', '11551', '11574', '11575', '11576', '11579', '11589', '11598', '12003', '12005', '12207']
+    
+    shoe_file_name_prefix = "shoe_"
+    watch_file_name_prefix = "watchdeviceId_"
+    survey_file_name_prefix = "survey_device_Id_"
+    
+    shoe_file_path_back_to_root = "../vinci_data/shoe/"
+    watch_file_path_back_to_root = "../vinci_data/watch/"
+    survey_file_path_back_to_root = "../vinci_data/survey/"
+    
+    shoe_file_path_from_root = "./vinci_data/shoe/"
+    watch_file_path_from_root = "./vinci_data/watch/"
+    survey_file_path_from_root = "./vinci_data/survey/"
--- unsupervised_k-means.py
+++ unsupervised_k-means.py
@@ -4,3 +4,23 @@ For now, it creates an unsupervised ML k-means algorithm for survey->survey_scor
 data
 """
 
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import json
+
+from helpers_aiders_and_conveniencers.file_names_and_prefixes import files_info
+
+# Get all panda data in a list
+panda_list = []
+numpy_list = []
+for survey_id in files_info.survey_ids:
+    string = files_info.survey_file_path_from_root + files_info.survey_file_name_prefix + survey_id
+    
+    temp_data_panda = pd.read_csv(string + '.csv')
+    temp_numpy = np.array(temp_data_panda)
+    panda_list.append(temp_data_panda) 
+    numpy_list.append(temp_numpy)   
+    
+# Get all survey_scores
