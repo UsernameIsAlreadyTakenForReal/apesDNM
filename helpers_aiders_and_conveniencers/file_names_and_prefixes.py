6a23fddc5e1538b27f052247021107cdfe38c1e6
Mr. Chis, innit <radu.cislianu@stud.acs.upb.ro>
2022-06-19 18:39:49 +0300

    [Unsupervised] Used k-Means for all data. Awful results

--- helpers_aiders_and_conveniencers/file_names_and_prefixes.py
+++ helpers_aiders_and_conveniencers/file_names_and_prefixes.py
@@ -18,3 +18,11 @@ class files_info:
     shoe_file_path_from_root = "./vinci_data/shoe/"
     watch_file_path_from_root = "./vinci_data/watch/"
     survey_file_path_from_root = "./vinci_data/survey/"
+
+    all_shoe_file_complete = '../vinci_data/shoe/all_shoe_data.csv'
+    all_watch_file_complete = '../vinci_data/watch/all_watch_data.csv'
+    all_survey_file_complete = '../vinci_data/survey/all_survey_data.csv'
+    
+    all_shoe_file_from_root_complete = './vinci_data/shoe/all_shoe_data.csv'
+    all_watch_file_from_root_complete = './vinci_data/watch/all_watch_data.csv'
+    all_survey_file_from_root_complete = './vinci_data/survey/all_survey_data.csv'
--- unsupervised_k-means.py
+++ unsupervised_k-means.py
@@ -8,32 +8,37 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
-
 from helpers_aiders_and_conveniencers.file_names_and_prefixes import files_info
 
-# Get all panda data in a list
-panda_list = []
-numpy_list = []
-for survey_id in files_info.survey_ids:
-    string = files_info.survey_file_path_from_root + files_info.survey_file_name_prefix + survey_id
-    
-    temp_data_panda = pd.read_csv(string + '.csv')
-    temp_numpy = np.array(temp_data_panda)
-    panda_list.append(temp_data_panda) 
-    numpy_list.append(temp_numpy)   
-    
+# Get all survey->elapsed_time data in a list
+data = pd.read_csv(files_info.all_survey_file_from_root_complete)
+data_numpy = np.array(data)
 
-no_of_clusters = 4
-# Plot initial clusters for all data
-for i in range(len(numpy_list)):
+elapsed_time_array = data_numpy[:,6]
+elapsed_time_array = elapsed_time_array.reshape(-1, 1)
+
+no_of_clusters = 7
+Kmean = KMeans(no_of_clusters)
+Kmean.fit(elapsed_time_array)
+
+print('Cluster centers: ', Kmean.cluster_centers_)
+
+plt.figure(1)
+plt.plot(Kmean.cluster_centers_, '*')
+plt.plot(elapsed_time_array)
+plt.show()
+
+
+# # Plot initial clusters for all data
+# for i in range(len(numpy_list)):
     
-    Kmean = KMeans(no_of_clusters)
-    X = numpy_list[i][:,6]
-    X = X.reshape(-1, 1)
-    Kmean.fit(X)  
+#     Kmean = KMeans(no_of_clusters)
+#     X = numpy_list[i][:,6]
+#     X = X.reshape(-1, 1)
+#     Kmean.fit(X)  
     
-    print ('for index ', i, ' we have ', Kmean.cluster_centers_)
-    plt.figure(i)
-    plt.plot(Kmean.cluster_centers_, '*')
-    plt.plot(X)
-    plt.show()
+#     print ('for index ', i, ' we have ', Kmean.cluster_centers_)
+#     plt.figure(i)
+#     plt.plot(Kmean.cluster_centers_, '*')
+#     plt.plot(X)
+#     plt.show()
