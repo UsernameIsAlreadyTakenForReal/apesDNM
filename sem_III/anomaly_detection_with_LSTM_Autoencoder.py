8fa646827df650bd53075b81e9066a24992e4521
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2023-04-08 12:12:37 +0300

    LSTM Changes

--- sem_III/anomaly_detection_with_LSTM_Autoencoder.py
+++ sem_III/anomaly_detection_with_LSTM_Autoencoder.py
@@ -16,17 +16,22 @@ from torch import nn, optim
 
 import torch.nn.functional as F
 
+import torch;
+torch.__version__
+
 
 ## This one is for anomaly_detection following the tutorial:
 ## https://curiousily.com/posts/time-series-anomaly-detection-using-lstm-autoencoder-with-pytorch-in-python/
 ## For each instruction in the tutorial, I included the comment above it, or something similar, so one can
-## ctrl+f and find it easily on the website, since the website uses some shit we do not posses
+## ctrl+f anad find it easily on the website, since the website uses some shit we do not posses
 
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 torch.manual_seed(RANDOM_SEED)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device("cpu")
+# device = "cpu"
 
 ###############################################################################
 ## --------------------------- Getting the datasets ---------------------------
@@ -125,7 +130,8 @@ val_df, test_df = train_test_split(
 ## ----------------------- Definition of classes and fct ----------------------
 ###############################################################################
 
-## We need to convert our examples into tensors, so we can use them to train our Autoencoder. 
+## We need to convert our examples into tensors, so we can use them to train 
+## our Autoencoder. 
 ## Let’s write a helper function for that:
     
 def create_dataset(df):
@@ -146,7 +152,8 @@ test_anomaly_dataset, _, _ = create_dataset(anomaly_df)
 ## LTSM Autoencoder
 
 ## We'll use the LSTM Autoencoder from this GitHub repo with some small tweaks. 
-## Our model's job is to reconstruct Time Series data. Let's start with the Encoder:
+## Our model's job is to reconstruct Time Series data.
+## Let's start with the Encoder:
 class Encoder(nn.Module):
 
   def __init__(self, seq_len, n_features, embedding_dim=64):
@@ -230,7 +237,8 @@ class RecurrentAutoencoder(nn.Module):
     return x
 
 
-## Our Autoencoder passes the input through the Encoder and Decoder. Let's create an instance of it:
+## Our Autoencoder passes the input through the Encoder and Decoder.
+## Let's create an instance of it:
 print("begin autoencoder model definitions")
 model = RecurrentAutoencoder(seq_len, n_features, 128)
 model = model.to(device)
@@ -296,13 +304,23 @@ def train_model(model, train_dataset, val_dataset, n_epochs):
 
 
 ## We'll get the version of the model with the smallest validation error. Let's do some training: 
+    
+f1_time = datetime.now()
+number_of_epochs = 20
+
 model, history = train_model(
   model, 
   train_dataset, 
   val_dataset, 
-  n_epochs=150
+  # n_epochs=150
+  n_epochs=number_of_epochs
 )
 
+f2_time = datetime.now()
+difference = f2_time - f1_time
+seconds_in_day = 24 * 60 * 60
+divmod(difference.days * seconds_in_day + difference.seconds, 60)
+device
 
 ax = plt.figure().gca()
 ax.plot(history['train'])
@@ -316,3 +334,90 @@ plt.show();
 ## Let's store the model for later use:
 MODEL_PATH = 'model.pth'
 torch.save(model, MODEL_PATH)
+
+print("It took {time} for {number} epochs".format(time=difference, number=number_of_epochs))
+
+
+###############################################################################
+## ------------------------ Test model chu chuuuuuuuuuuu ----------------------
+###############################################################################
+
+def predict(model, dataset):
+  predictions, losses = [], []
+  criterion = nn.L1Loss(reduction='sum').to(device)
+  with torch.no_grad():
+    model = model.eval()
+    for seq_true in dataset:
+      seq_true = seq_true.to(device)
+      seq_pred = model(seq_true)
+      loss = criterion(seq_pred, seq_true)
+      predictions.append(seq_pred.cpu().numpy().flatten())
+      losses.append(loss.item())
+  return predictions, losses
+
+_, losses = predict(model, train_dataset)
+sns.displot(losses, bins=50, kde=True)
+
+# Using the threshold, we can turn the problem into a simple binary classification task:
+# If the reconstruction loss for an example is below the threshold, we’ll classify it 
+#   as a normal heartbeat
+# Alternatively, if the loss is higher than the threshold, we’ll classify it as an anomaly
+THRESHOLD = 26
+
+# normal heartbeats
+predictions, pred_losses = predict(model, test_normal_dataset)
+sns.displot(pred_losses, bins=50, kde=True);
+correct = sum(l <= THRESHOLD for l in pred_losses)
+print(f'Correct normal predictions: {correct}/{len(test_normal_dataset)}')
+
+
+# anomalies
+anomaly_dataset = test_anomaly_dataset[:len(test_normal_dataset)]
+predictions, pred_losses = predict(model, anomaly_dataset)
+sns.displot(pred_losses, bins=50, kde=True);
+correct = sum(l > THRESHOLD for l in pred_losses)
+print(f'Correct anomaly predictions: {correct}/{len(anomaly_dataset)}')
+
+plt.figure(1)
+plt.plot(test_normal_dataset[0])
+plt.plot(predictions[0])
+plt.legend(['beat', 'prediction'])
+plt.title('Beat and prediction')
+plt.show()
+
+###############################################################################
+# CONFUSION MATRIX
+###############################################################################
+
+import matplotlib.pyplot as plt
+import numpy
+from sklearn import metrics
+
+actual = numpy.random.binomial(1,.9,size = 150)
+predicted = numpy.random.binomial(1,.9,size = 150)
+
+confusion_matrix = metrics.confusion_matrix(actual, predicted)
+
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = ["Anomaly", "Normal"])
+
+cm_display.plot()
+plt.show()
+
+
+
+from mlxtend.plotting import plot_confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
+class_names = ['Anomaly', 'Normal']
+
+binary1 = np.array([[123, 22],
+                    [0, 145]])
+
+fig, ax = plot_confusion_matrix(conf_mat=binary1,
+                                colorbar=True,
+                                show_absolute=True,
+                                class_names=class_names)
+plt.show()
+
+
