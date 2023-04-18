c2fed566b8c1a37b30b8d8d1f4b4475aa3268b88
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2023-04-19 00:15:22 +0300

    hmmnph

--- web/backend/main.py
+++ web/backend/main.py
@@ -1,36 +1,3 @@
-# from flask import Flask, request
-# from flask_cors import CORS, cross_origin
-# from flask_restful import Api, Resource
-
-# app = Flask(__name__)
-# cors = CORS(app)
-# app.config['CORS_HEADER'] = 'Content-Type'
-# api = Api(app)
-
-# class Testing(Resource):
-#     def get(self):
-#         return "this is the response of a get method call"
-#     def post(self):
-#         value = request.json.get('value')
-#         if value == "":
-#             value = 0
-#         new_val = {"value": int(value)*2}
-#         print(new_val)
-#         return new_val
-    
-# class Morbius(Resource):
-#     def get(self):
-#         return "watching him shout its morbin time! and starting to morb all the enemies was such a morbelous scene"
-
-# api.add_resource(Testing, "/testing")
-# api.add_resource(Morbius, "/morbin")
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-#############################################################################
-# this this kinda works but also acts a bit weird so hmmmm???
-
 import json
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS, cross_origin
@@ -39,39 +6,18 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADER'] = 'Content-Type'
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 @app.route('/morbin')
 def its_morbin_time():
-    return jsonify("this is a string")
+    return {
+        "message": "it's morbin time"
+    }
 
 @app.route('/double', methods=['GET', 'POST'])
 def get_double():
-    value = request.json.get('value')
-    if value == "": value = 0
-    new_val = {"msg": 2*int(value)}
-    return new_val
-
-@app.route('/upload', methods = ['GET', 'POST'])
-def upload_file():
-    # if request.method == 'POST':
-    #     f = request.files['file']
-    #     f.save(secure_filename(f.filename))
-    return "file uploaded successfully"
-
-@app.route('/uploaddd', methods = ['GET', 'POST'])
-def uploaddd_file():
-    record = json.loads(request.data)
-    with open('/tmp/data.txt', 'r') as f:
-        data = f.read()
-    if not data:
-        records = [record]
-    else:
-        records = json.loads(data)
-        records.append(record)
-    with open('/tmp/data.txt', 'w') as f:
-        f.write(json.dumps(records, indent=2))
-    return jsonify(record)
+    json1 = json.loads(json.dumps(request.get_json()))
+    print(json1["value1"] + "; " +  json1["value2"])
+    return { "message": "sum is " + str(int(json1["value1"]) + int(json1["value2"])) }
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- web/frontend/src/components/UploadComponent.js
+++ web/frontend/src/components/UploadComponent.js
@@ -8,17 +8,59 @@ export default function UploadComponent() {
   const [hover1, setHover1] = useState(false);
   const [text1, setText1] = useState("");
 
+  const [hover2, setHover2] = useState(false);
+  const [text2, setText2] = useState("");
+
+  const [hover3, setHover3] = useState(false);
+  const [text3, setText3] = useState("");
+
   function clearAllData() {
     setText1("");
+    setText2("");
+    setText3("");
+    document.getElementById("valueField1").value = "";
+    document.getElementById("valueField2").value = "";
   }
 
-  function onClick1Handle() {
-    fetch(BASE_URL + "morbin", {
+  async function onClick1Handle() {
+    const response = await fetch(BASE_URL + "morbin", {
       method: "get",
-    }).then((data) => {
-      console.log(data);
-      setText1(data);
     });
+    const jsonData = await response.json();
+    setText1(jsonData.message);
+  }
+
+  async function onClick2Handle() {
+    let data = {
+      value1: document.getElementById("valueField1").value
+        ? document.getElementById("valueField1").value
+        : "0",
+      value2: document.getElementById("valueField2").value
+        ? document.getElementById("valueField2").value
+        : "0",
+    };
+
+    console.log(data);
+
+    data = JSON.stringify(data);
+
+    const response = await fetch(BASE_URL + "double", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    });
+
+    const jsonData = await response.json();
+    setText2(jsonData.message);
+    console.log(jsonData);
+  }
+
+  async function onClick3Handle() {
+    const response = await fetch(BASE_URL + "morbin", {
+      method: "get",
+    });
+    const jsonData = await response.json();
+    setText1(jsonData.message);
   }
 
   return (
@@ -70,6 +112,74 @@ export default function UploadComponent() {
         </Divv>
 
         <Divv>from backend: {text1}</Divv>
+      </RowFlex>
+
+      <RowFlex justify="left">
+        <Divv>
+          <Button
+            style={{
+              background: hover2 === false ? "black" : "orange",
+              color: hover2 === false ? "white" : "black",
+              fontWeight: "bold",
+            }}
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => {
+              onClick2Handle();
+            }}
+            onMouseEnter={() => {
+              setHover2(true);
+            }}
+            onMouseLeave={() => {
+              setHover2(false);
+            }}
+          >
+            FETCH SUM
+          </Button>
+        </Divv>
+
+        <Divv margin="0px">
+          <TextFieldDivv>
+            <TextField variant="outlined" id="valueField1" />
+          </TextFieldDivv>
+
+          <TextFieldDivv>
+            <TextField variant="outlined" id="valueField2" />
+          </TextFieldDivv>
+        </Divv>
+
+        <Divv>from backend: {text2}</Divv>
+      </RowFlex>
+
+      <RowFlex justify="left">
+        <Divv>
+          <Button
+            style={{
+              background: hover2 === false ? "black" : "orange",
+              color: hover2 === false ? "white" : "black",
+              fontWeight: "bold",
+            }}
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => {
+              onClick3Handle();
+            }}
+            onMouseEnter={() => {
+              setHover2(true);
+            }}
+            onMouseLeave={() => {
+              setHover2(false);
+            }}
+          >
+            FETCH UPL
+          </Button>
+        </Divv>
+
+        <input type="file"></input>
+
+        <Divv>from backend: {text3}</Divv>
       </RowFlex>
     </>
   );
