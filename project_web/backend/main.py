4594d5dab1184679e869977be354a6a34049ef63
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2023-05-12 22:52:45 +0300

    i actually cannot believe chatgpt taught me web

--- project_web/backend/main.py
+++ project_web/backend/main.py
@@ -1,31 +1,34 @@
 from flask import Flask, request
 from flask_cors import CORS
 from flask.json import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/datatypes', methods=['GET', 'POST'])
+
+@app.route("/datatypes", methods=["GET", "POST"])
 def getDatasets():
     datasets = [
-        { "id": 1, "method": "EKG" },
-        { "id": 2, "method": "RAL" },
-        { "id": 3, "method": "IMG" },
-        { "id": 4, "method": "I don't know my data type..." },
+        {"id": 1, "method": "EKG"},
+        {"id": 2, "method": "RAL"},
+        {"id": 3, "method": "IMG"},
+        {"id": 4, "method": "I don't know my data type..."},
     ]
     datasets = jsonify(datasets)
     return datasets
 
-@app.route('/datatype', methods=['GET', 'POST'])
+
+@app.route("/datatype", methods=["GET", "POST"])
 def getSingularDataset():
     id = request.args["id"]
     method = ""
 
     datasets = [
-        { "id": 1, "method": "NN de mana ca saracii" },
-        { "id": 2, "method": "LSTM" },
-        { "id": 3, "method": "CUDA" },
-        { "id": 4, "method": "Some other method we copied from some other place" },
+        {"id": 1, "method": "NN de mana ca saracii"},
+        {"id": 2, "method": "LSTM"},
+        {"id": 3, "method": "CUDA"},
+        {"id": 4, "method": "Some other method we copied from some other place"},
     ]
 
     for item in datasets:
@@ -34,9 +37,7 @@ def getSingularDataset():
             method = item["method"]
 
     if found == False:
-        response = {
-            "error": "could not find method"
-        }
+        response = {"error": "could not find method"}
     else:
         # get data about method from database
         response = {
@@ -46,10 +47,21 @@ def getSingularDataset():
             "best_accuracy": 92.1,
             "time_elapsed_per_epoch_in_secs": 15.2,
             "epochs": 40,
-            "error": ""
+            "error": "",
         }
 
     return response
+
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    file = request.files["file"]
+    df = pd.read_csv(file)
+
+    print(df.shape)
+
+    return "File uploaded successfully"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- project_web/frontend/src/components/UploadComponent.js
+++ project_web/frontend/src/components/UploadComponent.js
@@ -25,6 +25,7 @@ const Inputt = styled.input`
 `;
 
 const BASE_URL = process.env.REACT_APP_BACKEND;
+console.log("url is", BASE_URL);
 
 export default function UploadComponent() {
   const [items, setItems] = useState([]);
@@ -34,6 +35,8 @@ export default function UploadComponent() {
   const [value, setValue] = useState("");
 
   const [hover2, setHover2] = useState(false);
+
+  const [selectedFile, setSelectedFile] = useState(null);
 
   async function getItems() {
     const response = await fetch(BASE_URL + "datatypes", {
@@ -52,6 +55,20 @@ export default function UploadComponent() {
     const data = await response.text();
     setSingularItem(data);
   }
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+    console.log(event.target.files[0]);
+  };
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+
+    const response = await fetch("/upload", {
+      method: "POST",
+      body: formData,
+    });
+  };
 
   useEffect(() => {
     getItems();
@@ -61,84 +78,90 @@ export default function UploadComponent() {
     <>
       <Divv>Select one of the existing types of data.</Divv>
 
-      <div style={{ flexDirection: "horizontal" }}>
-        <FormControl sx={{ width: "30%", margin: "20px" }} variant="outlined">
-          <InputLabel id="itemSelect">Data type</InputLabel>
-          <Select
-            labelId="itemId"
-            id="item"
-            label="itemSelect"
-            onChange={(event) => {
-              console.log("Now selected", event.target.value);
-              setValue(event.target.value);
-            }}
-          >
-            <MenuItem key="0" value="" disabled>
-              Choose a method
-            </MenuItem>
-            {items.map((item) => {
-              return (
-                <MenuItem key={item.id} value={item.id}>
-                  {item.method}
-                </MenuItem>
-              );
-            })}
-          </Select>
-          {/* {value === "" ? (
+      <form>
+        <div style={{ flexDirection: "horizontal" }}>
+          <FormControl sx={{ width: "30%", margin: "20px" }} variant="outlined">
+            <InputLabel id="itemSelect">Data type</InputLabel>
+            <Select
+              labelId="itemId"
+              id="item"
+              label="itemSelect"
+              onChange={(event) => {
+                console.log("Now selected", event.target.value);
+                setValue(event.target.value);
+              }}
+            >
+              <MenuItem key="0" value="" disabled>
+                Choose a method
+              </MenuItem>
+              {items.map((item) => {
+                return (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.method}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+            {/* {value === "" ? (
             <FormHelperText>
               Please select a data type from the ones previously processed
             </FormHelperText>
           ) : (
             <></>
           )} */}
-        </FormControl>
+          </FormControl>
+          <Divv top="0px">
+            <Labell
+              class="custom-file-upload"
+              style={{
+                display: "inline-block",
+                background: hover2 === false ? "white" : "grey",
+                color: hover2 === false ? "black" : "white",
+                transition: "color 0.4s linear",
+                transition: "background 0.4s linear",
+              }}
+              onMouseEnter={() => setHover2(true)}
+              onMouseLeave={() => setHover2(false)}
+            >
+              <Inputt
+                type="file"
+                onChange={(event) => handleFileSelect(event)}
+              />
+              Click here to upload the file...
+            </Labell>
+          </Divv>
+        </div>
+
         <Divv top="0px">
-          <Labell
-            class="custom-file-upload"
+          <Button
             style={{
-              display: "inline-block",
-              background: hover2 === false ? "white" : "grey",
-              color: hover2 === false ? "black" : "white",
-              transition: "color 0.4s linear",
-              transition: "background 0.4s linear",
+              background: hover1 === false ? "black" : "orange",
+              color: hover1 === false ? "white" : "black",
+              fontWeight: "bold",
             }}
-            onMouseEnter={() => setHover2(true)}
-            onMouseLeave={() => setHover2(false)}
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={() => {
+              if (selectedFile === null) {
+                console.log("You need to select a file first...");
+                return;
+              }
+              console.log("sending over file", selectedFile.name);
+
+              handleSubmit();
+            }}
+            onMouseEnter={() => {
+              setHover1(true);
+            }}
+            onMouseLeave={() => {
+              setHover1(false);
+            }}
           >
-            <Inputt type="file" />
-            Click here to upload the file...
-          </Labell>
-        </Divv>{" "}
-      </div>
-
-      <Divv top="0px">
-        <Button
-          style={{
-            background: hover1 === false ? "black" : "orange",
-            color: hover1 === false ? "white" : "black",
-            fontWeight: "bold",
-          }}
-          variant="contained"
-          color="primary"
-          size="large"
-          onClick={() => {
-            if (value === "") return;
-            console.log("Fetching request for method", value);
-
-            getSingularItem();
-          }}
-          onMouseEnter={() => {
-            setHover1(true);
-          }}
-          onMouseLeave={() => {
-            setHover1(false);
-          }}
-        >
-          Upload data
-        </Button>
-      </Divv>
-
-      {/* {singularItem.length ? JSON.stringify(singularItem) : <></>} */}
+            Upload data
+          </Button>
+        </Divv>
+      </form>
     </>
   );
 }
