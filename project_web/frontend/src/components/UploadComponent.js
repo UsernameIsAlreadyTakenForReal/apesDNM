3fc042a598cffb5e1daee0f3cac6f88c0c208510
Daniel Dumitru <daniel.dumitru1105@stud.acs.upb.ro>
2023-05-30 23:06:08 +0300

    lol?

--- project_web/frontend/src/components/UploadComponent.js
+++ project_web/frontend/src/components/UploadComponent.js
@@ -35,9 +35,6 @@ export default function UploadComponent() {
 
   const [selectedFiles, setSelectedFiles] = useState([]);
 
-  const [datasetClasses, setDatasetClasses] = useState([]);
-  const [datasetNormalClass, setDatasetNormalClass] = useState("");
-
   const [saveDataCheckbox, setSaveDataCheckbox] = useState(false);
   const [labeledRadioValue, setLabeledRadioValue] = useState("yes");
   const [isSupervisedCheckbox, setIsSupervisedCheckbox] = useState(false);
@@ -49,6 +46,8 @@ export default function UploadComponent() {
   const [classesTextfieldsError, setClassesTextfieldsError] = useState(false);
   const [classesTextfieldsErrorMessage, setClassesTextfieldsErrorMessage] =
     useState(false);
+
+  const [normalClass, setNormalClass] = useState("");
 
   const [loading, setLoading] = useState(false);
 
@@ -130,6 +129,17 @@ export default function UploadComponent() {
   }
 
   function handleClassChange() {
+    let classes = [];
+    classesTextfields.forEach((textfield) => {
+      if (document.getElementById("class-textfield" + textfield).value) {
+        classes.push(
+          document.getElementById("class-textfield" + textfield).value
+        );
+      }
+    });
+
+    console.log(classes);
+
     setClasses(classes);
     return classes;
   }
@@ -184,14 +194,7 @@ export default function UploadComponent() {
     }
 
     // classes logic
-    let classes = [];
-    classesTextfields.forEach((textfield) => {
-      if (document.getElementById("class-textfield" + textfield).value) {
-        classes.push(
-          document.getElementById("class-textfield" + textfield).value
-        );
-      }
-    });
+    let classes = handleClassChange();
 
     if (classes.length !== classesTextfields.length) {
       setClassesTextfieldsError(true);
@@ -199,18 +202,31 @@ export default function UploadComponent() {
       return;
     }
 
-    // normal class value logic
-    const normalClass = document.getElementById("normal-class-field").value;
-
-    if (normalClass === "") {
-      setNormalClassError(true);
-      setNormalClassErrorMessage("a normal class value must be provided");
+    if (new Set(classes).size !== classes.length) {
+      setClassesTextfieldsError(true);
+      setClassesTextfieldsErrorMessage("remove duplicate values");
       return;
     }
 
-    if (classes.includes(normalClass) === false) {
+    // normal class value logic
+    // const normalClass = document.getElementById("normal-class-field").value;
+
+    // if (normalClass === "") {
+    //   setNormalClassError(true);
+    //   setNormalClassErrorMessage("a normal class value must be provided");
+    //   return;
+    // }
+
+    // if (classes.includes(normalClass) === false) {
+    // setNormalClassError(true);
+    // setNormalClassErrorMessage("normal class must be among classes");
+    // return;
+    // }
+
+    // normal class
+    if (normalClass === "") {
       setNormalClassError(true);
-      setNormalClassErrorMessage("normal class must be among classes");
+      setNormalClassErrorMessage("normal class must be selected");
       return;
     }
 
@@ -567,11 +583,7 @@ export default function UploadComponent() {
         {classesTextfields.map((textfield) => {
           return (
             <TextFieldFlex style={{ marginTop: "10px" }}>
-              <Divv
-                size="22.5px"
-                style={{ margin: "25px", width: "55%" }}
-                color={labeledRadioValue === "yes" ? "black" : "lightgray"}
-              >
+              <Divv size="22.5px" style={{ margin: "25px", width: "55%" }}>
                 {textfield === 1 ? (
                   <>
                     <span
@@ -579,6 +591,7 @@ export default function UploadComponent() {
                       onClick={() => {
                         if (classesTextfields.length === 1) return;
                         setClassesTextfields(classesTextfields.slice(0, -1));
+                        handleClassChange();
                       }}
                     >
                       ((-)){" "}
@@ -591,6 +604,7 @@ export default function UploadComponent() {
                           ...classesTextfields,
                           classesTextfields.length + 1,
                         ]);
+                        handleClassChange();
                       }}
                     >
                       {" "}
@@ -607,9 +621,9 @@ export default function UploadComponent() {
                   width: "45%",
                   display: "flex",
                   margin: "25px",
-                  marginTop: textfield === 1 ? "25px" : "00px",
+                  marginTop: textfield === 1 ? "25px" : "0px",
                   marginBottom:
-                    textfield === classesTextfields.length ? "25px" : "10px",
+                    textfield === classesTextfields.length ? "25px" : "0px",
                 }}
                 error={classesTextfieldsError}
                 helperText={
@@ -621,6 +635,7 @@ export default function UploadComponent() {
                 id={"class-textfield" + textfield}
                 variant="outlined"
                 label={"class #" + textfield}
+                onChange={() => handleClassChange()}
               />
             </TextFieldFlex>
           );
@@ -636,31 +651,23 @@ export default function UploadComponent() {
             error={normalClassError}
           >
             <InputLabel id="data-type-select">class type</InputLabel>
-            <Select label="data-type-select" onChange={(event) => {}}>
+            <Select
+              label="data-type-select"
+              onChange={(event) => {
+                setNormalClass(event.target.value);
+                console.log(event.target.value);
+              }}
+            >
               <MenuItem key="0" value="" disabled>
                 choose the normal class
               </MenuItem>
-              {classesTextfields.map((item) => {
+              {classes.map((item, index) => {
                 return (
                   <MenuItem
-                    key={item}
-                    value={
-                      document.getElementById("class-textfield" + item)
-                        ? document.getElementById("class-textfield" + item)
-                            .value === ""
-                          ? "no value yet at #" + item
-                          : document.getElementById("class-textfield" + item)
-                              .value
-                        : "alabala"
-                    }
+                    key={index}
+                    value={item === "" ? "no value at " + (index + 1) : item}
                   >
-                    {document.getElementById("class-textfield" + item)
-                      ? document.getElementById("class-textfield" + item)
-                          .value === ""
-                        ? "no value yet at #" + item
-                        : document.getElementById("class-textfield" + item)
-                            .value
-                      : "undefined"}
+                    {item === "" ? "no value at " + (index + 1) : item}
                   </MenuItem>
                 );
               })}
