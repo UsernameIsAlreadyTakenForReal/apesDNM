6565a28139d3ffb9dc598da33b973f0c51ae5ad9
Mr. Chis, innit <radu.cislianu@stud.acs.upb.ro>
2023-06-23 23:24:43 +0300

    Don't keep the ssh on git

--- Cloud_Requisites/EC2_startup.sh
+++ Cloud_Requisites/EC2_startup.sh
@@ -180,13 +180,8 @@ echo "---- STEP: Git connection key configuration ----"
 echo "------------------------------------------------"
 mkdir ~/.ssh
 touch ~/.ssh/id_ed25519
-echo "-----BEGIN OPENSSH PRIVATE KEY-----
-b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
-QyNTUxOQAAACAvs7xAz/nkwTcXEXzfxFkoDf8CRUNx6r5gO9JsKSMyKQAAAJjrFfN56xXz
-eQAAAAtzc2gtZWQyNTUxOQAAACAvs7xAz/nkwTcXEXzfxFkoDf8CRUNx6r5gO9JsKSMyKQ
-AAAEBdqvJra+i2nbNekLj+75Jq1nSu8JXntsKf8ZjogKCzFC+zvEDP+eTBNxcRfN/EWSgN
-/wJFQ3HqvmA70mwpIzIpAAAAEGFwZWRubUBnbWFpbC5jb20BAgMEBQ==
------END OPENSSH PRIVATE KEY-----" >> ~/.ssh/id_ed25519
+aws s3 cp s3://apesdnm-s3/startup/apes_ssh_key.txt ~/startup/apes_ssh_key.txt
+cat ~/startup/apes_ssh_key.txt >> ~/.ssh/id_ed25519
 sudo chmod 700 ~/.ssh/
 sudo chmod 600 ~/.ssh/id_ed25519
 
